9a5d598f479c6560e79b744df0042e6f2f9d7997
Minwei Yao <dzrematt@gmail.com>
2019-05-10 00:53:33 +1000

    Class Body created with 2 constructors

---
+++ proj0/Body.java
@@ -0,0 +1,30 @@
+/** creat a Body class
+*/
+public class Body {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+	/** first constructor to create an object with given arguments */
+	public Body(double xP, double yP, double xV, 
+					double yV, double m, String img) {
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+	/** second constructor to create an object identiy to a given object of Body */
+	public Body(Body b) {
+		xxPos = b.xxPos;
+		yyPos = b.yyPos;
+		xxVel = b.xxVel;
+		yyVel = b.yyVel;
+		mass = b.mass;
+		imgFileName = b.imgFileName;
+	}
+
+}
