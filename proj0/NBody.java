3e9eaf43ded8dc0b4e0d7f92b1bef294a7f9ca91
Minwei Yao <dzrematt@gmail.com>
2019-05-14 23:43:12 +1000

    NBody class can read radius and bodies from file now

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,7 +1,25 @@
 /** simulator */
 public class NBody {
-	public static double readRadius() {
+	public static double readRadius(String filepath) {
+		In in = new In(filepath);
 
+		/* Obtain radius from the given txt file. */
+		in.readInt();
+		double radius = in.readDouble();
+		return radius;
+	}	
+	public static Body[] readBodies(String filepath) {
+		In in = new In(filepath);
+		int bodyNumber = in.readInt();
+		Body[] bodies = new Body[bodyNumber];
+		in.readDouble();
+		for (int i = 0; i < bodyNumber; i++) {
+			bodies[i] = new Body(in.readDouble(), in.readDouble(), in.readDouble(), 
+				in.readDouble(), in.readDouble(), in.readString());
+		}
+		return bodies;
 	}
+
+
 }
 
