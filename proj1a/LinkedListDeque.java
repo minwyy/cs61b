e14d240b0cc48ce64c791acf894519faaf961c36
Minwei Yao <dzrematt@gmail.com>
2019-05-20 23:08:04 +1000

    basic form of Doubly Linked List

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,6 +1,51 @@
-/** create a "Double Ended Queue" using both lists and arrays,
+/** Create a "Double Ended Queue" using both lists and arrays,
  * based on Double Linked List.
  */
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+    /** Variables of Deque class.
+     *  This list working as a middleman
+     *  as IntNode is the naked list storing data.
+     */
+    private IntNode sentinel;
+    private int size;
+
+    /** A class for naked list "IntNode" to store data.
+     */
+    public class IntNode {
+        public IntNode prev;
+        public T item;
+        public IntNode next;
+
+        /** Create a NEW IntNode with given parameters. */
+        public IntNode(IntNode p, T i, IntNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    /** Create an empty linked list deque. */
+    public LinkedListDeque() {
+        sentinel = new Integer(11, null);
+    }
+
+
+    /** add an time of type T to the front of the deque. */
+    public void addFirst(T item) {
+        sentinel = new IntNode(item, sentinel);
+    }
+    public void addLast(T item) {
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
