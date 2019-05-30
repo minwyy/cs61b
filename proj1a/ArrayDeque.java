935301f3f5f8d4fb36f45312430016451cc5985d
Minwei Yao <dzrematt@gmail.com>
2019-05-31 01:13:18 +1000

    Complete methods for ArrayDeque, not tested yet.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -35,5 +35,47 @@ public class ArrayDeque<T> {
         return size;
     }
 
+    /** Prints the items in the deque from first to last, separated by a space.
+      * Prints out a new line when all items printed. */
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + " ");
+        }
+        System.out.println("");
+    }
 
+    /** Removes and returns the last item at the back of the deque. If no such item exists, return null. */
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        } else {
+            T last = items[size-1];
+            items[size-1] = null;
+            size -= 1;
+            return last;
+        }
+    }
+
+    /** Removes and returns the first item at the back of the deque. If no such item exists, return null. */
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        } else {
+            T first = items[0];
+            for (int i = 0; i < size - 1; i++) {
+                items[i] = items[i+1];
+            }
+            items[size-1] = null;
+            return first;
+        }
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. */
+    public T get(int index) {
+        if (index < size) {
+            return items[index];
+        } else {
+            return null;
+        }
+    }
 }
