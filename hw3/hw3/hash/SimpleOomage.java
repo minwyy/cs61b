727f14db893c5eb1912a3d6af620d82bc728c0bd
Minwei Yao <dzrematt@gmail.com>
2019-08-01 14:29:15 +1000

    completed equals()

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -14,13 +14,19 @@ public class SimpleOomage implements Oomage {
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
-        return false;
+        if (o == null) return false;
+        if (o.getClass() != this.getClass()) return false;
+        SimpleOomage c = (SimpleOomage) o;
+        if (red == c.red && green == c.green && blue == c.blue) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /* Uncomment this method after you've written
+    /** Uncomment this method after you've written
        equals and failed the testHashCodeAndEqualsConsistency
-       test.
+       test. */
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
@@ -29,7 +35,7 @@ public class SimpleOomage implements Oomage {
             // TODO: Write a perfect hash function for Simple Oomages.
             return 0;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -39,7 +39,7 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
+
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -47,7 +47,7 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
     /* TODO: Uncomment this test after you finish haveNiceHashCodeSpread in OomageTestUtility */
     /*@Test
