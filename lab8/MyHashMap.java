fab00b33488f34cd2a7a0402136b0cc17a015de1
Minwei Yao <dzrematt@gmail.com>
2019-08-08 23:47:15 +1000

    completed lab8

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -6,6 +6,7 @@ public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     private int capac; //HashMap capacity
     private int number; //Number of pairs in the HashMap
+    private double lf; //Load factor of MyHashMap
     private HashSet<K> allKeys; //Stores keys for iteration
     private ArrayList[] ht; //ArrayList to store hash table
 
@@ -22,6 +23,7 @@ public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     public MyHashMap(int initialSize, double loadFactor) {
         capac = initialSize;
         number = 0;
+        lf = loadFactor;
         allKeys = new HashSet<K>();
         ht = new ArrayList[capac];
         for (int i = 0; i < capac; i++) {
@@ -37,16 +39,26 @@ public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         this(DEF_CAPACITY, DEF_LOADFACTOR);
     }
 
+    /** Hash the key value into relative bucket. */
+    private int hashIndex(K key) {
+        return (key.hashCode() & 0x7FFFFFFF) % capac;
+    }
+
     /** Removes all of the mappings from this map. */
     @Override
     public void clear() {
-
+        number = 0;
+        allKeys = new HashSet<K>();
+        ht = new ArrayList[capac];
+        for (int i = 0; i < capac; i++) {
+            ht[i] = new ArrayList<pairKV>();
+        }
     }
 
     /** Returns true if this map contains a mapping for the specified key. */
     @Override
     public boolean containsKey(K key) {
-
+        return allKeys.contains(key);
     }
 
     /**
@@ -55,13 +67,38 @@ public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-
+        if (key == null) throw new IllegalArgumentException("invalid key value");
+        ArrayList<pairKV> al = ht[hashIndex(key)];
+        if (al != null) {
+            for (pairKV x : al) {
+                if (x.key.equals(key)) {
+                    return x.value;
+                }
+            }
+        }
+        return null;
     }
 
     /** Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
+        return number;
+    }
 
+
+    /** Resize the array for more values. */
+    public void resize() {
+        boolean needResize = ((double) number / (double) capac) > lf;
+        if (needResize) {
+            MyHashMap<K, V> temp = new MyHashMap(capac * 2);
+            for (ArrayList<pairKV> al : ht) {
+                for (pairKV x : al) {
+                    temp.put(x.key, x.value);
+                }
+            }
+            this.ht = temp.ht;
+            this.capac = temp.capac;
+        }
     }
 
     /**
@@ -71,19 +108,34 @@ public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-
+        if (key == null) {
+            throw new IllegalArgumentException("Please input valid key.");
+        }
+        ArrayList<pairKV> al = ht[hashIndex(key)];
+        if (!containsKey(key)) {
+            al.add(new pairKV(key, value));
+            allKeys.add(key);
+            number += 1;
+            } else {
+            for (pairKV x : al) {
+                if (x.key == key) {
+                    x.value = value;
+                }
+            }
+        }
+        resize();
     }
 
     /** Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet(){
-
+        return allKeys;
     }
 
 
     @Override
     public Iterator<K> iterator() {
-
+        return allKeys.iterator();
     }
 
 
