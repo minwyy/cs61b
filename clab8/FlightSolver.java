7a82227165adb5adaa1d9b5f3d76313067ef8c41
Minwei Yao <dzrematt@gmail.com>
2019-08-10 18:54:02 +1000

    Completed FlightSolver.java

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,44 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+    private Comparator<Flight> startComparator = (i, j) -> i.startTime() - j.startTime();
+    private Comparator<Flight> endComparator = (i, j) -> i.endTime() - j.endTime();
+    private PriorityQueue<Flight> minStart;
+    private PriorityQueue<Flight> minEnd;
+    private int sum = 0;
+    private int maxSum = 0;
 
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        minStart = new PriorityQueue<Flight>(flights.size(), startComparator);
+        minEnd = new PriorityQueue<Flight>(flights.size(), endComparator);
+        /** Adds elements in Array into the PriorityQueues. */
+        for (Flight x : flights) {
+            minStart.add(x);
+            minEnd.add(x);
+        }
+
+        /** Works out the maximum sum. */
+        while (!minStart.isEmpty()){
+            Flight takingOff = minStart.peek();
+            Flight landing = minEnd.peek();
+            /** Planes on the air. */
+            if (takingOff.startTime <= landing.endTime) {
+                sum += takingOff.passengers();
+                minStart.poll();
+            /** Plane lands. */
+            } else {
+                sum -= landing.passengers();
+                minEnd.poll();
+            }
+            if (maxSum < sum) {
+                maxSum = sum;
+            }
+        }
+
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        return maxSum;
     }
 
 }
