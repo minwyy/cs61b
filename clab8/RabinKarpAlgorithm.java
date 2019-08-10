a0071365513c59e7ad9e82972af5faad638cbf4b
Minwei Yao <dzrematt@gmail.com>
2019-08-10 19:17:15 +1000

    tried and gave up clab8

--- clab8/RabinKarpAlgorithm.java
+++ clab8/RabinKarpAlgorithm.java
@@ -6,6 +6,7 @@ public class RabinKarpAlgorithm {
      * This method will return -1 if no matching substring is found, or if the input is invalid.
      */
     public static int rabinKarp(String input, String pattern) {
+
         return -1;
     }
 
